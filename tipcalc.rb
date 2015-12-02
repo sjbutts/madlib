ca8e05ac834cf98307c48f79217e0cc32651c72c
Shanika <sjbutts@gmail.com>
2015-12-02 13:32:32 -0500

    first commit

---
+++ tipcalc.rb
@@ -0,0 +1,24 @@
+
+
+
+puts 'What is the total of your bill?'
+
+bill = gets.chomp
+tip = bill.to_f * 0.20
+final= bill.to_f + tip.to_f
+
+puts 'Your total bill including 20% tip is ' + final.to_s + '.'
+
+#puts bill.to_f * 0.20
+
+#puts 'Your total bill will be: ' + bill.to_s + tip.to_s
+
+
+
+
+
+
+ 
+
+ 
+	
