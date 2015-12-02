de66d1792d6a2b20b5fc7f303587750e93dea88e
Shanika <sjbutts@gmail.com>
2015-12-02 13:22:36 -0500

    First commit

---
+++ madlib.rb
@@ -0,0 +1,53 @@
+def number(integer)
+	puts number = ''
+
+
+
+
+
+
+puts 'Enter a plural noun'
+
+plural_noun1 = gets.chomp
+
+puts 'Enter a noun '
+
+noun1 = gets.chomp
+
+puts 'Enter a noun'
+
+noun2 = gets.chomp
+
+puts 'Enter an adjective'
+
+adjective1= gets.chomp
+
+puts 'Enter a plural noun'
+
+plural_noun2 = gets.chomp
+
+puts 'Enter a plural noun'
+
+plural_noun3 = gets.chomp
+
+puts 'Enter a noun'
+
+noun3 = gets.chomp
+
+puts 'Enter a plural noun'
+
+plural_noun4 = gets.chomp
+
+puts 'Enter a verb'
+
+verb1 = gets.chomp
+
+puts 'Enter an adjective'
+
+adjective2= gets.chomp
+
+puts 'Enter a number'
+
+number1 = gets.chomp
+
+puts 'In the early 2000s, crossword ' + plural_noun1 + ' only appeared in children\'s books. Today, ' + noun1 + ' puzzles are in almost every ' + noun2 + ' printed in the U.S. and throughout the whole ' + adjective1 + ' world. More people do crossword puzzles than smoke ' + plural_noun2 + ' or drink ' + plural_noun3 + '.  Some fanatics are known to do their puzzles even before they wash their ' + noun3 + ', brush their ' + plural_noun4 + ', or ' + verb1 + ' their breakast.  Another ' + adjective2 + ' word puzzle book has '  + number1 + ' pages.'
